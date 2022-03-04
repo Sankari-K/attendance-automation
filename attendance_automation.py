df96d7b9855b66fa20698d20a99833fb29abe6d2
Sankari Karthik <sankarikarthik03#gmail.com>
2022-03-04 20:59:09 +0530

    Initial Commit

---
+++ attendance_automation.py
@@ -0,0 +1,67 @@
+# Imports
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from secrets import *
+from credentials import *
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+# "Constants" required
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+
+# To get rid of the error messages printed out
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(PATH,  options=options)
+
+# Go to the webpage
+driver.get("https://forms.gle/CKtq3Xp3rVBpz7JR8") #TODO: Get user input and validate
+print("Navigating to webpage..", driver.title)
+
+# TO load DOM
+time.sleep(1)
+
+# To login to google
+search = driver.find_elements_by_class_name("CwaK9")
+for i in search:
+    try:
+        i.click()
+    except:
+        pass    
+
+driver.implicitly_wait(5)
+print("Will try to search")
+
+user_name = driver.find_element_by_id("identifierId")
+user_name.send_keys(username)
+
+next = driver.find_element_by_class_name("VfPpkd-vQzf8d").click()
+pass_word = driver.find_element_by_name("password")
+pass_word.send_keys(password)
+time.sleep(2)
+done = driver.find_element_by_class_name("VfPpkd-vQzf8d").click()
+
+
+# To fill out the form
+
+print("before searching")
+
+time.sleep(5)
+cnt = 0
+print("not started input searching yet")
+
+
+for i in range(len(creds)):
+    details = driver.find_element_by_css_selector(selector[i])
+    details.click()
+    details.send_keys(creds[i])
+print("Done") 
+submit = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span/span').click()
+
+
+#close the tab
+#time.sleep(5)
+#driver.close()
